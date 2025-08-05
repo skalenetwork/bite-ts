fff80f7a0e207a2c69acce01de56c5f262e78d0e
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2025-08-05 21:52:49 +0100

    Revert tx copy changes

--- src/core/encrypt.ts
+++ src/core/encrypt.ts
@@ -50,23 +50,22 @@ export async function encryptTransaction(
     endpoint: string
 ): Promise<Transaction> {
     try {
-        const txCopy: Transaction = { ...tx };
-        const validatedTx = validateAndExtractTransactionFields(txCopy);
+        const validatedTx = validateAndExtractTransactionFields(tx);
         const txTo = validatedTx.to;
         let txData = validatedTx.data;
 
         // append 'to' field to end of data
         txData += txTo;
 
-        txCopy.data = await encryptMessage(txData, endpoint);
-        txCopy.to = constants.BITE_ADDRESS;
+        tx.data = await encryptMessage(txData, endpoint);
+        tx.to = constants.BITE_ADDRESS;
 
         // Set default gasLimit if not set
-        if (!txCopy.gasLimit) {
-            txCopy.gasLimit = constants.DEFAULT_GAS_LIMIT;
+        if (!tx.gasLimit) {
+            tx.gasLimit = constants.DEFAULT_GAS_LIMIT;
         }
 
-        return txCopy;
+        return tx;
     } catch (error) {
         logger.error('Error encrypting transaction:', error);
         throw error;
@@ -82,23 +81,22 @@ export async function encryptTransaction(
  */
 export async function encryptTransactionMockup(tx: Transaction): Promise<Transaction> {
     try {
-        const txCopy: Transaction = { ...tx };
-        const validatedTx = validateAndExtractTransactionFields(txCopy);
+        const validatedTx = validateAndExtractTransactionFields(tx);
         const txTo = validatedTx.to;
         let txData = validatedTx.data;
 
         // append 'to' field to end of data
         txData += txTo;
 
-        txCopy.data = await encryptMessageMockup(txData);
-        txCopy.to = constants.BITE_ADDRESS;
+        tx.data = await encryptMessageMockup(txData);
+        tx.to = constants.BITE_ADDRESS;
 
         // Set default gasLimit if not set
-        if (!txCopy.gasLimit) {
-            txCopy.gasLimit = constants.DEFAULT_GAS_LIMIT;
+        if (!tx.gasLimit) {
+            tx.gasLimit = constants.DEFAULT_GAS_LIMIT;
         }
 
-        return txCopy;
+        return tx;
     } catch (error) {
         logger.error('Error encrypting transaction (mockup):', error);
         throw error;
